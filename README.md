f5e2e689f983d7d8acf2a33752aac6dc6a7be0cf
bitsper2nd <bitsper2nd@outlook.com>
2024-05-10 15:07:29 -0400

    feat: powershell profile and setup script.

---
+++ README.md
@@ -0,0 +1,40 @@
+# 🚀 PowerShell Profile
+
+A stylish and functional PowerShell profile that looks and feels almost as good as a Linux terminal.
+
+## ✨ Features
+
+A smarter PowerShell so you always know which command to run next.
+
+- Input History
+- Next Word Prediction
+- Smart Quotes
+- Argument Highlighter
+- Auto Bracket Pairing
+- Custom Prompt
+
+## ⚡ Installation
+
+Execute the following command in an elevated PowerShell window to install the PowerShell profile:
+
+```
+irm "https://github.com/bitsper2nd/powershell-profile/main/Microsoft.PowerShell_profile.ps1" | iex
+```
+
+The script will also install the following cli tools from Winget:
+- [RipGrep](https://github.com/BurntSushi/ripgrep), A clone of grep.
+- [sd](https://github.com/chmln/sd), A sed alternative.
+- [fd](https://github.com/sharkdp/fd), An alternative to find.
+- [starship](https://github.com/starship/starship), A customizable prompt.
+- [gsudo](https://github.com/gerardog/gsudo), Sudo for Windows.
+- [zoxide](https://github.com/ajeetdsouza/zoxide), A smarter cd command.
+- [bat](https://github.com/sharkdp/bat), A cat clone.
+- [tlrc](https://github.com/tldr-pages/tlrc), Cheatsheets for commands.
+- [lsd](https://github.com/lsd-rs/lsd), The next gen ls command.
+
+## Credits
+
+- Chris Titus for his [PowerShell Profile repository](https://github.com/ChrisTitusTech/powershell-profile)
+- Scott Hanselman for his [Powershell Profile Gist](https://gist.github.com/shanselman/25f5550ad186189e0e68916c6d7f44c3)
+
+Now, enjoy your enhanced PowerShell experience!
---
+++ setup.ps1
@@ -0,0 +1,73 @@
+# Ensure the script can run with elevated privileges
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "Please run this script as an Administrator!"
+    break
+}
+
+# Function to test internet connectivity
+function Test-InternetConnection {
+    try {
+        $testConnection = Test-Connection -ComputerName www.google.com -Count 1 -ErrorAction Stop
+        return $true
+    }
+    catch {
+        Write-Warning "Internet connection is required but not available. Please check your connection."
+        return $false
+    }
+}
+
+# Check for internet connectivity before proceeding
+if (-not (Test-InternetConnection)) {
+    break
+}
+
+# Profile creation or update
+if (!(Test-Path -Path $PROFILE -PathType Leaf)) {
+    try {
+        # Detect Version of PowerShell & Create Profile directories if they do not exist.
+        $profilePath = ""
+        if ($PSVersionTable.PSEdition -eq "Core") { 
+            $profilePath = "$env:userprofile\Documents\Powershell"
+        }
+        elseif ($PSVersionTable.PSEdition -eq "Desktop") {
+            $profilePath = "$env:userprofile\Documents\WindowsPowerShell"
+        }
+
+        if (!(Test-Path -Path $profilePath)) {
+            New-Item -Path $profilePath -ItemType "directory"
+        }
+
+        Invoke-RestMethod https://github.com/bitsper2nd/powershell-profile/main/Microsoft.PowerShell_profile.ps1 -OutFile $PROFILE
+        Write-Host "The profile @ [$PROFILE] has been created."
+        Write-Host "If you want to add any persistent components, please do so at [$profilePath\Profile.ps1] as there is an updater in the installed profile which uses the hash to update the profile and will lead to loss of changes"
+    }
+    catch {
+        Write-Error "Failed to create or update the profile. Error: $_"
+    }
+}
+else {
+    try {
+        Get-Item -Path $PROFILE | Move-Item -Destination "oldprofile.ps1" -Force
+        Invoke-RestMethod https://github.com/bitsper2nd/powershell-profile/main/Microsoft.PowerShell_profile.ps1 -OutFile $PROFILE
+        Write-Host "The profile @ [$PROFILE] has been created and old profile removed."
+        Write-Host "Please back up any persistent components of your old profile to [$HOME\Documents\PowerShell\Profile.ps1] as there is an updater in the installed profile which uses the hash to update the profile and will lead to loss of changes"
+    }
+    catch {
+        Write-Error "Failed to backup and update the profile. Error: $_"
+    }
+}
+
+# Winget Install
+try {
+    winget install -e --id "Starship.Starship" && "ajeetdsouza.zoxide" && "gerardog.gsudo" && "lsd-rs.lsd" && "BurntSushi.ripgrep.MSVC" && "Git.MinGit" && "sharkdp.bat" && "sharkdp.fd" && "chmln.sd" && "tldr-pages.tlrc"
+}
+catch {
+    Write-Error "Failed to install Command Line tools. Error: $_"
+}
+
+# Final check and message to the user
+if ((Test-Path -Path $PROFILE) -and (winget list --id "Starship.Starship" && "ajeetdsouza.zoxide" && "gerardog.gsudo" && "lsd-rs.lsd" && "BurntSushi.ripgrep.MSVC" && "Git.MinGit" && "sharkdp.bat" && "sharkdp.fd" && "chmln.sd" && "tldr-pages.tlrc")) {
+    Write-Host "Setup completed successfully. Please restart your PowerShell session to apply changes."
+} else {
+    Write-Warning "Setup completed with errors. Please check the error messages above."
+}
